eea764df0ba9a5c736e18c6ab92394f47252f072
user <valeriazhukovskaya789@gmail.com>
2023-11-29 20:32:08 +0200

    patient  registration page - done

--- script/script.js
+++ script/script.js
@@ -10,23 +10,68 @@ document.addEventListener('DOMContentLoaded', function () {
     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
     formBtn.addEventListener('click', function (event) {
-        if (!emailRegex.test(emailInput.value)) {
-            alert('Please enter a valid email address');
-            event.preventDefault();
-        }
+        validateEmail(emailInput);
+        validatePassword(passwordInput);
+        validateConfirmPassword(confirmPasswordInput);
 
-        if (!passwordRegex.test(passwordInput.value)) {
-            alert('Please enter a valid password. It should contain at least 1 digit, 1 uppercase letter, 1 lowercase letter, and have a minimum length of 8 characters.');
-            event.preventDefault();
-        }
+        event.preventDefault(); // Остановить отправку формы, если есть ошибки
+    });
 
-        if (passwordInput.value !== confirmPasswordInput.value) {
-            alert('Passwords do not match');
-            event.preventDefault();
+    // Функция для проверки корректности email
+    function validateEmail(input) {
+        const errorMessage = document.getElementById('email-error');
+        if (!emailRegex.test(input.value)) {
+            errorMessage.textContent = 'Please enter a valid email address';
+            errorMessage.style.color = 'red';
+            input.style.borderColor = 'red';
+        } else {
+            errorMessage.textContent = '';
+            input.style.borderColor = '';
         }
+    }
+
+    // Функция для проверки корректности пароля
+    function validatePassword(input) {
+        const errorMessage = document.getElementById('password-error');
+        if (!input.value.match(passwordRegex)) {
+            errorMessage.textContent = 'Please enter a valid password. It should contain at least 1 digit, 1 uppercase letter, 1 lowercase letter, and have a minimum length of 8 characters.';
+            errorMessage.style.color = 'red';
+            input.style.borderColor = 'red';
+        } else {
+            errorMessage.textContent = '';
+            input.style.borderColor = '';
+        }
+    }
+
+    // Функция для проверки совпадения паролей
+    function validateConfirmPassword(input) {
+        const errorMessage = document.getElementById('confirmPassword-error');
+        const password = passwordInput.value;
+        if (input.value !== password) {
+            errorMessage.textContent = 'Passwords not match';
+            errorMessage.style.color = 'red';
+            input.style.borderColor = 'red';
+        } else {
+            errorMessage.textContent = '';
+            input.style.borderColor = '';
+        }
+    }
+
+    // Добавление обязательного поля
+    const inputs = document.querySelectorAll('input[required]');
+    inputs.forEach(function (input) {
+        input.addEventListener('input', function () {
+            if (!input.value.trim()) {
+                input.nextElementSibling.textContent = 'This field is required';
+                input.nextElementSibling.style.color = 'red';
+                input.style.borderColor = 'red';
+            } else {
+                input.nextElementSibling.textContent = '';
+                input.style.borderColor = ''; // Вернуть стандартный цвет границы
+            }
+        });
     });
 });
-
 
 //Пути для получения данных с фейкового сервера:
 // http://localhost:3000/doctors - данные о докторах
